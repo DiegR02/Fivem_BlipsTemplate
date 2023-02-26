e9b96dd37d663d966fa8b03e14f31bbb8a151431
Diego <02diegorf@gmail.com>
2023-02-26 15:42:21 +0100

    Project commited

---
+++ client/client.lua
@@ -0,0 +1,19 @@
+local blips = {
+    {title="Hospital", colour=75, id=61, scale= 1.0, x = -256.82, y = 6334.4, z = 32.8},
+  }
+      
+
+Citizen.CreateThread(function()
+
+    for _, info in pairs(blips) do
+      info.blip = AddBlipForCoord(info.x, info.y, info.z)
+      SetBlipSprite(info.blip, info.id)
+      SetBlipDisplay(info.blip, 4)
+      SetBlipScale(info.blip, info.scale)
+      SetBlipColour(info.blip, info.colour)
+      SetBlipAsShortRange(info.blip, true)
+	    BeginTextCommandSetBlipName("STRING")
+      AddTextComponentString(info.title)
+      EndTextCommandSetBlipName(info.blip)
+    end
+end)
